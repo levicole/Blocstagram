94f6985f8d2b9028603bfa7969407857b52a6165
Levi Kennedy <lckennedy@gmail.com>
2015-06-08 16:31:39 -0500

    Checkpoint 26 completed

--- Blocstagram/ImagesTableViewController.h
+++ Blocstagram/ImagesTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  ImagesTableViewController.h
 //  Blocstagram
 //
 //  Created by Levi Kennedy on 6/8/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
-
+@interface ImagesTableViewController : UITableViewController
+@property (nonatomic, strong) NSMutableArray *images;
 @end
-
