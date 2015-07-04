613166ae063463500013045a9d41394c80627649
Levi Kennedy <lckennedy@gmail.com>
2015-07-04 08:53:31 -0500

    I'm stuck

--- Blocstagram/ImagesTableViewController.m
+++ Blocstagram/ImagesTableViewController.m
@@ -15,6 +15,7 @@
 #import "MediaFullScreenViewController.h"
 #import "CameraViewController.h"
 #import "ImageLibraryViewController.h"
+#import "PostToInstagramViewController.h"
 
 @interface ImagesTableViewController () <MediaTableCellViewDelegate, CameraViewControllerDelegate, ImageLibraryViewControllerDelegate>
 
@@ -339,6 +340,16 @@
 
 #pragma mark - Camera, CameraViewControllerDelegate and ImageLibraryViewControllerDelegate
 
+- (void) handleImage:(UIImage *)image withNavigationController:(UINavigationController *)nav {
+    if (image) {
+        PostToInstagramViewController *postVC = [[PostToInstagramViewController alloc] initWithImage:image];
+        
+        [nav pushViewController:postVC animated:YES];
+    } else {
+        [nav dismissViewControllerAnimated:YES completion:nil];
+    }
+}
+
 - (void) cameraPressed:(UIBarButtonItem *) sender {
     UIViewController *imageVC;
     
@@ -360,23 +371,11 @@
 }
 
 - (void) cameraViewController:(CameraViewController *)cameraViewController didCompleteWithImage:(UIImage *)image {
-    [cameraViewController dismissViewControllerAnimated:YES completion:^{
-        if (image) {
-            NSLog(@"Got an image!");
-        } else {
-            NSLog(@"Closed without an image.");
-        }
-    }];
+    [self handleImage:image withNavigationController:cameraViewController.navigationController];
 }
 
 - (void) imageLibraryViewController:(ImageLibraryViewController *)imageLibraryViewController didCompleteWithImage:(UIImage *)image {
-    [imageLibraryViewController dismissViewControllerAnimated:YES completion:^{
-        if (image) {
-            NSLog(@"Got an image!");
-        } else {
-            NSLog(@"Closed without an image.");
-        }
-    }];
+    [self handleImage:image withNavigationController:imageLibraryViewController.navigationController];
 }
 
 @end
---
+++ Blocstagram/PostToInstagramViewController.h
@@ -0,0 +1,15 @@
+//
+//  PostToInstagramViewController.h
+//  Blocstagram
+//
+//  Created by Levi Kennedy on 7/2/15.
+//  Copyright (c) 2015 Levi Kennedy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostToInstagramViewController : UICollectionViewController
+
+- (instancetype) initWithImage:(UIImage *)sourceImage;
+
+@end
